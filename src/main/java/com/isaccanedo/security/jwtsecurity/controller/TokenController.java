853e5d1ae92f154002331120b2f3d7db0e496a49
isaccanedo <isaccanedo@gmail.com>
2020-08-29 09:26:11 -0300

    JWT in Spring Security for Spring Boot

---
+++ src/main/java/com/isaccanedo/security/jwtsecurity/controller/TokenController.java
@@ -0,0 +1,28 @@
+package com.isaccanedo.security.jwtsecurity.controller;
+
+import com.isaccanedo.security.jwtsecurity.model.JwtUser;
+import com.isaccanedo.security.jwtsecurity.security.JwtGenerator;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/token")
+public class TokenController {
+
+
+    private JwtGenerator jwtGenerator;
+
+    public TokenController(JwtGenerator jwtGenerator) {
+        this.jwtGenerator = jwtGenerator;
+    }
+
+    @PostMapping
+    public String generate(@RequestBody final JwtUser jwtUser) {
+
+        return jwtGenerator.generate(jwtUser);
+
+    }
+}
